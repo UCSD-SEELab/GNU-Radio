2eafd10c7cba3f9f471a5e0dcfc595725d229e81
Jason Ma <jasonma5501@gmail.com>
2018-06-29 18:43:25 -0700

    added most current bit error testing module

--- gr_modules/bertest.py
+++ gr_modules/bertest.py
@@ -24,7 +24,8 @@ INTERVAL = 1
 
 RX = 0
 TX = 1
-MODE = TX
+MANUAL = 2
+MODE = MANUAL
 
 #TODO figure out what to TX, do this in a way that is easy to change
 TX_PATTERN = b'\xff\x00\xff\x00'
@@ -49,19 +50,27 @@ def main():
   total_bytes = 0
   test_start = time.time()
   start = time.time()
+
+  first_packet = True
   #connect to socket server as client. Print total number of packets received over time
   
   if MODE == RX:
     try:
       while True:
         data, addr = sdr_s.recvfrom(120)
+        
+        if first_packet:
+          test_start = time.time()
+          first_packet = False
+        
         total_bytes += len(data)
-        print("[main] [RX] " + str(data))
+        #print("[main] [RX] " + str(data))
         #print("[main] [Rx] " + str(int.from_bytes(data, byteorder='big')))
 
         now = time.time()
         if(now - start > INTERVAL):
-          print("[main] Total RX: " + str(total_bytes) + " b")
+          total_time = now - test_start
+          print("[main] Total RX: " + str(total_bytes) + " b | Time: " + str(total_time))
           start = time.time()
     except KeyboardInterrupt:
       print("[main] Ctrl+C detected. Ending test")
@@ -80,6 +89,39 @@ def main():
         if(now - start > INTERVAL):
           print("[main] Total TX: " + str(total_bytes) + " b")
           start = time.time()
+        else:
+          time.sleep(0.01)
+    except KeyboardInterrupt:
+      print("[main] Ctrl+C detected. Ending test")
+
+      total_time = time.time() - test_start
+
+      print("[main] Test results:")
+      print("[main] Total bytes TXed: " + str(total_bytes) + " b | Total Time: " + str(total_time) + "s")
+
+  elif MODE == MANUAL:
+    try:
+      while True:
+
+       data, addr = sdr_s.recvfrom(120)
+        
+       if first_packet:
+         test_start = time.time()
+         first_packet = False
+        
+       total_bytes += len(data)
+
+       now = time.time()
+       if(now - start > INTERVAL):
+         total_time = now - test_start
+         print("[main] Total RX: " + str(total_bytes) + " b | Time: " + str(total_time))
+         start = time.time()
+
+       if time.time() - test_start >= 15.0:
+         total_time = now - test_start
+         print("[main] Test results:")
+         print("[main] Total bytes RXed: " + str(total_bytes) + " b | Total Time: " + str(total_time) + "s")
+         break
 
     except KeyboardInterrupt:
       print("[main] Ctrl+C detected. Ending test")
